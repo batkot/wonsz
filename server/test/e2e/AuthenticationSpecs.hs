2d3682f3e551232f3707b327991b474471686861
Tomasz Batko <batkot@protonmail.com>
2020-09-15 12:06:23 +0200

    Extract WonszApp to separate module in e2e tests

--- server/test/e2e/AuthenticationSpecs.hs
+++ server/test/e2e/AuthenticationSpecs.hs
@@ -4,7 +4,6 @@ module AuthenticationSpecs
     ( test_tests
     ) where
 
-import Control.Concurrent (forkIO, killThread, ThreadId)
 import Data.Either
 
 import qualified Data.ByteString.Lazy as BSL
@@ -14,76 +13,33 @@ import Test.Tasty (TestTree, testGroup, withResource)
 import Test.Tasty.HUnit (testCase, (@?=), (@?))
 import Test.Tasty.QuickCheck (testProperty, ioProperty)
 import Test.QuickCheck (Arbitrary(..), getPrintableString)
-import Network.Wai.Handler.Warp as Warp
 
-import Servant (Proxy(..), (:<|>)(..))
-import Servant.Auth.Server (generateKey, JWT)
-import Servant.Client 
-import Servant.Auth.Client
-import Network.HTTP.Client (newManager, defaultManagerSettings)
 import Network.HTTP.Types.Status (status401)
 
-import Wonsz.Server (app, Api)
-import Wonsz.Server.Authentication (AuthToken, LoginRequest(..), rawToken)
-import Wonsz.Server.Season (SeasonOverview(..))
+import Servant.Client 
+import Servant.Auth.Client
+import Resources.WonszApp 
 
-import Wonsz.Users (UserMonad(..), User(..))
-
-import Control.Monad.Identity (IdentityT, runIdentityT)
-
-makeWonszAppResource :: IO (Port, ThreadId)
-makeWonszAppResource = do
-    (port, socket) <- Warp.openFreePort
-    jwt <- generateKey
-    threadId <- forkIO $ Warp.runSettingsSocket defaultSettings socket (app runIdentityT jwt)
-    return (port, threadId)
-
-instance Monad m => UserMonad (IdentityT m) where
-  getUser userName = return . Just $ User 1 userName "password"
-
-freeWonszAppResource :: (Port, ThreadId) -> IO ()
-freeWonszAppResource = killThread . snd
-
--- type Api auth = "api" :> SeasonApi auth :<|> "auth" :> AuthApi auth
---
-testApi :: Proxy (Api '[JWT])
-testApi = Proxy
-
-data WonszClient = WonszClient
-    { login :: LoginRequest -> ClientM AuthToken
-    , renewToken :: Token -> ClientM AuthToken
-    , getOverview :: Token -> ClientM SeasonOverview
-    , env :: ClientEnv
-    }
-
-createApiClient :: Port -> IO WonszClient
-createApiClient port = do
-    let overviewClient :<|> (renewTokenClient :<|> loginClient) :<|> _ = client testApi
-    baseUrl <- parseBaseUrl "http://localhost"
-    manager <- newManager defaultManagerSettings
-    let clientEnv = mkClientEnv manager (baseUrl { baseUrlPort = port })
-    return $ WonszClient loginClient renewTokenClient overviewClient clientEnv
+import Wonsz.Server.Authentication (rawToken)
 
 test_tests :: TestTree
-test_tests = 
-    withResource makeWonszAppResource freeWonszAppResource $ \x ->
-        withResource (fst <$> x >>= createApiClient) (const  (return ())) $ \client -> 
+test_tests = withWonszApp $ \client -> 
             testGroup "Login workflow tests" 
                 [ testCase "Given wrong credentails should raise 401" $ do
                     c <- client
-                    Left (FailureResponse _ (Response responseStatus _ _ _)) <- runClientM ((login c) (LoginRequest "dupa" "dupa")) (env c)
+                    Left (FailureResponse _ (Response responseStatus _ _ _)) <- runClientM ((login c) invalidLoginRequest) (env c)
                     responseStatus @?= status401
 
                 , testCase "Given good credentials should return working token" $ do
                     c <- client
-                    Right authToken <- runClientM ((login c) (LoginRequest "dupa" "password")) (env c)
+                    Right authToken <- runClientM ((login c) validLoginRequest) (env c)
                     let token = Token . BSL.toStrict . rawToken $ authToken
                     result <- runClientM (getOverview c token) (env c)
                     isRight result @? "Should get result"
 
                 , testCase "Token renewal return new working token" $ do 
                     c <- client
-                    Right authToken <- runClientM ((login c) (LoginRequest "dupa" "password")) (env c)
+                    Right authToken <- runClientM ((login c) validLoginRequest) (env c)
                     let token = Token . BSL.toStrict . rawToken $ authToken
                     Right renewedToken <- runClientM (renewToken c token) (env c)
                     let newToken = Token . BSL.toStrict . rawToken $ renewedToken
---
+++ server/test/e2e/Resources/WonszApp.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE DataKinds #-}
+
+module Resources.WonszApp 
+    ( withWonszApp
+
+    , makeWonszAppResource
+    , freeWonszAppResource
+    , createApiClient
+    , WonszClient(..)
+
+    , validLoginRequest 
+    , invalidLoginRequest 
+    ) where
+
+import Test.Tasty (TestTree, testGroup, withResource)
+
+import Control.Monad.Identity (IdentityT, runIdentityT)
+import Control.Concurrent (forkIO, killThread, ThreadId)
+
+import Network.Wai.Handler.Warp as Warp
+import Network.HTTP.Client (newManager, defaultManagerSettings)
+
+import Servant (Proxy(..), (:<|>)(..))
+import Servant.Client 
+import Servant.Auth.Server (generateKey, JWT)
+import Servant.Auth.Client
+
+import Wonsz.Server (app, Api)
+import Wonsz.Server.Authentication (AuthToken, LoginRequest(..), rawToken)
+import Wonsz.Server.Season (SeasonOverview(..))
+
+import Wonsz.Users (UserMonad(..), User(..))
+
+withWonszApp :: (IO WonszClient -> TestTree) -> TestTree
+withWonszApp tests = 
+    withResource makeWonszAppResource freeWonszAppResource $ \x ->
+        withResource (fst <$> x >>= createApiClient) (const  (return ())) tests
+
+makeWonszAppResource :: IO (Port, ThreadId)
+makeWonszAppResource = do
+    (port, socket) <- Warp.openFreePort
+    jwt <- generateKey
+    threadId <- forkIO $ Warp.runSettingsSocket defaultSettings socket (app runIdentityT jwt)
+    return (port, threadId)
+
+freeWonszAppResource :: (Port, ThreadId) -> IO ()
+freeWonszAppResource = killThread . snd
+
+testApi :: Proxy (Api '[JWT])
+testApi = Proxy
+
+data WonszClient = WonszClient
+    { login :: LoginRequest -> ClientM AuthToken
+    , renewToken :: Token -> ClientM AuthToken
+    , getOverview :: Token -> ClientM SeasonOverview
+    , env :: ClientEnv
+    }
+
+createApiClient :: Port -> IO WonszClient
+createApiClient port = do
+    let overviewClient :<|> (renewTokenClient :<|> loginClient) :<|> _ = client testApi
+    baseUrl <- parseBaseUrl "http://localhost"
+    manager <- newManager defaultManagerSettings
+    let clientEnv = mkClientEnv manager (baseUrl { baseUrlPort = port })
+    return $ WonszClient loginClient renewTokenClient overviewClient clientEnv
+
+----
+userName :: String
+userName = "whatever"
+
+goodPassword :: String
+goodPassword = "password"
+
+badPassword :: String
+badPassword = "notPassword"
+
+validLoginRequest :: LoginRequest
+validLoginRequest = LoginRequest userName goodPassword
+
+invalidLoginRequest :: LoginRequest 
+invalidLoginRequest = LoginRequest userName badPassword
+
+instance Monad m => UserMonad (IdentityT m) where
+  getUser userName = return . Just $ User 1 userName goodPassword
